01aeb7daf39c0b3ab4a889c9b6dd6425a06fd8fa
Shun <ter0123540@gmail.com>
2022-07-04 15:35:05 +0800

    finish al the function

--- todo.js
+++ todo.js
@@ -4,6 +4,8 @@ const userToken = localStorage.getItem("userToken");
 const userName = localStorage.getItem("userNickname");
 const config = { headers: { Authorization: userToken } };
 let data = [];
+let checkStatus = "";
+let position = 1;
 
 // button
 const addBtn = document.querySelector(".plus");
@@ -15,8 +17,8 @@ const clearAll = document.querySelector(".clear-all");
 const logout = document.querySelector(".logout");
 
 // display
+const main = document.querySelector("main");
 const enterBar = document.querySelector(".enter-bar");
-const owner = document.querySelector(".owner");
 const todoBoard = document.querySelector(".todo-board");
 
 // function
@@ -28,6 +30,7 @@ function init() {
       .get(url, config)
       .then((res) => {
         data = res.data.todos;
+        data.reverse();
         render();
       })
       .catch((err) => {
@@ -42,17 +45,65 @@ function redirect() {
 
 function render() {
   let allContent = "";
-  data.forEach((item) => {
-    allContent += `<li class="todo-item bottom-line d-flex align-items-center position-relative">
-    <label class="label d-flex align-items-center w-100 cursor-pointer">
-      <input type="checkbox" class="d-none"/>
-      <span class="check-mark position-relative cursor-pointer me-2"></span>
-      <span class="content">${item.content}</span>
-    </label>
-    <i class="trash-icon fa-solid fa-trash-can cursor-pointer" data-id=${item.id}></i>
-  </li>`;
-  });
+
+  if (position === 1) {
+    data.forEach((item) => {
+      if (item.completed_at) {
+        checkStatus = "checked";
+      }
+      allContent += `
+        <li class="todo-item bottom-line d-flex align-items-center position-relative" data-id=${item.id}>
+          <label class="label d-flex align-items-center w-100 cursor-pointer">
+            <input type="checkbox" class="d-none" ${checkStatus}/>
+            <span class="check-mark position-relative cursor-pointer me-2"></span>
+            <span class="content">${item.content}</span>
+          </label>
+          <span class="circle d-flex justify-content-center align-items-center cursor-pointer">
+            <i class="fa-solid fa-trash-can text-white pointer-events no-event"></i>
+          </span>
+        </li>
+      `;
+      checkStatus = "";
+    });
+  } else if (position === 2) {
+    data.forEach((item) => {
+      if (!item.completed_at) {
+        allContent += `
+          <li class="todo-item bottom-line d-flex align-items-center position-relative" data-id=${item.id}>
+            <label class="label d-flex align-items-center w-100 cursor-pointer">
+              <input type="checkbox" class="d-none" ${checkStatus}/>
+              <span class="check-mark position-relative cursor-pointer me-2"></span>
+              <span class="content">${item.content}</span>
+            </label>
+            <span class="circle d-flex justify-content-center align-items-center cursor-pointer">
+              <i class="fa-solid fa-trash-can text-white pointer-events no-event"></i>
+            </span>
+          </li>
+        `;
+      }
+    });
+  } else {
+    data.forEach((item) => {
+      if (item.completed_at) {
+        checkStatus = "checked";
+        allContent += `
+          <li class="todo-item bottom-line d-flex align-items-center position-relative" data-id=${item.id}>
+            <label class="label d-flex align-items-center w-100 cursor-pointer">
+              <input type="checkbox" class="d-none" ${checkStatus}/>
+              <span class="check-mark position-relative cursor-pointer me-2"></span>
+              <span class="content">${item.content}</span>
+            </label>
+            <span class="circle d-flex justify-content-center align-items-center cursor-pointer">
+              <i class="fa-solid fa-trash-can text-white pointer-events no-event"></i>
+            </span>
+          </li>
+        `;
+      }
+      checkStatus = "";
+    });
+  }
   todoBoard.innerHTML = allContent;
+  main.classList.remove("no-event");
 }
 
 function time() {
@@ -95,6 +146,7 @@ function userLogout() {
 }
 
 function addTask() {
+  if (enterBar.value.length < 1) return;
   const obj = { todo: {} };
   obj.todo.content = enterBar.value;
   enterBar.value = "";
@@ -102,7 +154,7 @@ function addTask() {
   axios
     .post(url, obj, config)
     .then((res) => {
-      data.unshift(res.data);
+      data.push(res.data);
       render();
     })
     .catch((err) => {
@@ -110,17 +162,96 @@ function addTask() {
     });
 }
 
+function toggleStatus(e) {
+  main.classList.add("no-event");
+  if (e.target.nodeName !== "INPUT") return;
+  const targetId = e.target.parentNode.parentNode.getAttribute("data-id");
+  const targetObj = data.filter((item) => item.id === targetId)[0];
+  axios
+    .patch(url + targetId + "/toggle", "", config)
+    .then((res) => {
+      data.splice(data.indexOf(targetObj), 1, res.data);
+      render();
+    })
+    .catch((err) => {
+      alert("錯誤");
+      render();
+    });
+}
+
 function deleteTask(e) {
-  if (!e.target.classList.contains("trash-icon")) return;
-  const targetId = e.target.getAttribute("data-id");
+  main.classList.add("no-event");
+  if (!e.target.classList.contains("circle")) return;
+  const targetId = e.target.parentNode.getAttribute("data-id");
+
+  const targetItem = data.filter((item) => item.id === targetId)[0];
   axios
     .delete(url + targetId, config)
     .then((res) => {
-      console.log(res);
+      data.splice(data.indexOf(targetItem), 1);
+      render();
     })
     .catch((err) => {
       console.log(err);
     });
+}
+
+function deleteFinished(e) {
+  if (data.length < 1) return;
+  main.classList.add("no-event");
+  data.forEach((item) => {
+    if (item.completed_at) {
+      axios
+        .delete(url + item.id, config)
+        .then((res) => {
+          data.splice(data.indexOf(item), 1);
+          render();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
+}
+
+function deleteAll(e) {
+  if (data.length < 1) return;
+  main.classList.add("no-event");
+  data.forEach((item) => {
+    axios
+      .delete(url + item.id, config)
+      .then((res) => {
+        data.splice(0, 1);
+        render();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+}
+
+function checkAll(e) {
+  position = 1;
+  allTag.classList.add("active");
+  yetTag.classList.remove("active");
+  finishedTag.classList.remove("active");
+  render();
+}
+
+function checkYet(e) {
+  position = 2;
+  yetTag.classList.add("active");
+  allTag.classList.remove("active");
+  finishedTag.classList.remove("active");
+  render();
+}
+
+function checkDone(e) {
+  position = 3;
+  finishedTag.classList.add("active");
+  allTag.classList.remove("active");
+  yetTag.classList.remove("active");
+  render();
 }
 
 logout.addEventListener("click", userLogout);
@@ -134,6 +265,17 @@ enterBar.addEventListener("keyup", (e) => {
 
 todoBoard.addEventListener("click", deleteTask);
 
+clearFinishedBtn.addEventListener("click", deleteFinished);
+
+clearAll.addEventListener("click", deleteAll);
+
+todoBoard.addEventListener("click", toggleStatus);
+
+allTag.addEventListener("click", checkAll);
+
+yetTag.addEventListener("click", checkYet);
+
+finishedTag.addEventListener("click", checkDone);
+
 init();
-owner.textContent = `${userName}的清單`;
 setInterval(time, 1000);
